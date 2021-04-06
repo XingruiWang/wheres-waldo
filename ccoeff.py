1eab10577cf343c0fcaefb37aaf45c4fe62d2887
Xingrui Wang <XingrWang@163.com>
2021-04-06 18:59:52 +0800

    Update ccoeff.py

--- ccoeff.py
+++ ccoeff.py
@@ -4,24 +4,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-'''
-import cv2 as cv
-import numpy as np
-from matplotlib import pyplot as plt
-img_rgb = cv.imread('mario.png')
-img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-template = cv.imread('mario_coin.png',0)
-w, h = template.shape[::-1]
-res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-cv.imwrite('res.png',img_rgb)
-
-'''
-
-
 def argument(img):
     '''
     argument data to different direction

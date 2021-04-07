5c79c9be3ad24f8861e90a4e263a5bfc9548a45f
Xingrui Wang <XingrWang@163.com>
2021-04-07 10:20:47 +0800

    add threshold

--- ccoeff.py
+++ ccoeff.py
@@ -3,24 +3,6 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 
-
-'''
-import cv2 as cv
-import numpy as np
-from matplotlib import pyplot as plt
-img_rgb = cv.imread('mario.png')
-img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-template = cv.imread('mario_coin.png',0)
-w, h = template.shape[::-1]
-res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-cv.imwrite('res.png',img_rgb)
-'''
-
-
 def argument(img):
     '''
     argument data to different direction
@@ -109,7 +91,6 @@ def template_matching(img, template_dir, threshold = 0.8, return_ori=False, vis=
     if isinstance(img, str):
         img = cv2.imread(img)
 
-#     img = img[:180, :, :]
 
     templates = []
     for t_file in os.listdir(template_dir):
